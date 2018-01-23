abefbcf39595506cf179cf731e32cfa48e8ee8f5
Justin Daniel <justinddaniel@gmail.com>
2018-01-23 20:49:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,14 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let codearray = [];
+
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function(e) {
+    for (let i = 0, l = code.length; i < l; i++) {
+      codearray.push(e.which);
+
+      if (codearray === code) {alert('Congratuations! You entered the Konami Code!')}
+    }
+  })
 
 }
